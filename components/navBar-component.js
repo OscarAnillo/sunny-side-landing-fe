7528bd74e0a041cdb145c26a33bb86d95ccb3715
oscar anillo <oscarani14@gmail.com>
2022-08-31 16:13:44 -0500

    add navBar and routes for app

---
+++ components/navBar-component.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import Link from 'next/link';
+
+export default function NavBar(){
+    const [ showMenu, setShowMenu] = useState(false);
+
+    const clickHandler = () => {
+        setShowMenu(!showMenu);
+    }
+
+    return (
+        <section className='section-nav'>
+            <nav className='nav-row'>
+                <div>
+                    <img src="/images/logo.svg" alt="" />
+                </div>
+                <div className='desktop-nav'>
+                    <ul>
+                        <li><Link href="/about">About</Link></li>
+                        <li><Link href="/services">Services</Link></li>
+                        <li><Link href="/projects">Projects</Link></li>
+                        <li><Link href="/contact">Contact</Link></li>
+                    </ul>
+                </div>
+                <div onClick={clickHandler}>
+                    <img src="/images/icon-hamburger.svg"alt ="" />
+                </div>
+                {showMenu && 
+                <div>
+                <ul>
+                        <li><Link href="/about">About</Link></li>
+                        <li><Link href="/services">Services</Link></li>
+                        <li><Link href="/projects">Projects</Link></li>
+                        <li><Link href="/contact">Contact</Link></li>
+                    </ul>
+                </div>
+                }
+            </nav>
+        </section>
+    )
+}
