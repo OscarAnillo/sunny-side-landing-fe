b9e68e7e7e2a4d958ec7ac179acb0498331f40bf
oscar anillo <oscarani14@gmail.com>
2022-08-31 21:40:54 -0500

    add footer, styles and react icons package

--- components/footer.js
+++ components/footer.js
@@ -1,14 +1,21 @@
+import { GrFacebook, GrInstagram, GrTwitter, GrPinterest } from 'react-icons/gr'
+
 export default function Footer(){
     return (
         <footer>
             <div>
                 <h1>SunnySide</h1>
-                <div>
+                <div className='footer-row'>
                     <p>About</p>
                     <p>Services</p>
                     <p>Projects</p>
                 </div>
-                <div>
+                <div className='socials'>
+                    
+                        <GrFacebook />
+                        <GrInstagram />
+                        <GrTwitter />
+                        <GrPinterest />
                     
                 </div>
             </div>
