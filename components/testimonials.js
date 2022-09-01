381b1e561df703364d0ef3994cf3aa569c0a26ca
oscar anillo <oscarani14@gmail.com>
2022-09-01 11:34:36 -0500

    fix mobile images to no-repeat

--- components/testimonials.js
+++ components/testimonials.js
@@ -1,12 +1,11 @@
 import data from "../data/data-testimonials.json";
 
 export default function Testimonials(){
-    console.log(data)
     return (
         <div className="testimonials">
             <h1>Client Testimonials</h1>
             {data.map(item => (
-                <div>
+                <div key={item.name}>
                     <img src={item.avatar} alt="" />
                     <p>{item.legend}</p>
                     <h3>{item.name}</h3>
