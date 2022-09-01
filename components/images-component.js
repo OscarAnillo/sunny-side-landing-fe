fbf1ee0f9a8916b1c703aef5b47498241467206f
oscar anillo <oscarani14@gmail.com>
2022-09-01 13:47:49 -0500

    add tablet view with queries

--- components/images-component.js
+++ components/images-component.js
@@ -2,12 +2,16 @@ export default function Images(){
     return (
         <div className="images-row">
             <div>
-                <img src="/images/mobile/image-gallery-milkbottles.jpg" alt="" />
-                <img src="/images/mobile/image-gallery-cone.jpg" alt="" />
+                <img src="/images/mobile/image-gallery-milkbottles.jpg" alt="" className="mobile-img" />
+                <img src="/images/mobile/image-gallery-cone.jpg" alt="" className="mobile-img" />
+                <img src="/images/desktop/image-gallery-milkbottles.jpg" alt="" className="desktop-img" />
+                <img src="/images/desktop/image-gallery-cone.jpg" alt="" className="desktop-img" />
             </div>
             <div>
-                <img src="/images/mobile/image-gallery-orange.jpg" alt="" />
-                <img src="/images/mobile/image-gallery-sugar-cubes.jpg" alt="" />
+                <img src="/images/mobile/image-gallery-orange.jpg" alt="" className="mobile-img" />
+                <img src="/images/mobile/image-gallery-sugar-cubes.jpg" alt="" className="mobile-img" />
+                <img src="/images/desktop/image-gallery-orange.jpg" alt="" className="desktop-img" />
+                <img src="/images/desktop/image-gallery-sugarcubes.jpg" alt="" className="desktop-img" />
             </div>    
         </div>
     )
