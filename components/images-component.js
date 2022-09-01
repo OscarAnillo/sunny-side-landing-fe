eec1aa01a9a28fd1bda14e335ce2c7ca4871592d
oscar anillo <oscarani14@gmail.com>
2022-09-01 18:10:49 -0500

    add desktop view

--- components/images-component.js
+++ components/images-component.js
@@ -1,18 +1,18 @@
 export default function Images(){
     return (
-        <div className="images-row">
-            <div>
+        <section>
+            <div className="images-row-mobile">
                 <img src="/images/mobile/image-gallery-milkbottles.jpg" alt="" className="mobile-img" />
-                <img src="/images/mobile/image-gallery-cone.jpg" alt="" className="mobile-img" />
-                <img src="/images/desktop/image-gallery-milkbottles.jpg" alt="" className="desktop-img" />
-                <img src="/images/desktop/image-gallery-cone.jpg" alt="" className="desktop-img" />
-            </div>
-            <div>
                 <img src="/images/mobile/image-gallery-orange.jpg" alt="" className="mobile-img" />
+                <img src="/images/mobile/image-gallery-cone.jpg" alt="" className="mobile-img" />
                 <img src="/images/mobile/image-gallery-sugar-cubes.jpg" alt="" className="mobile-img" />
-                <img src="/images/desktop/image-gallery-orange.jpg" alt="" className="desktop-img" />
-                <img src="/images/desktop/image-gallery-sugarcubes.jpg" alt="" className="desktop-img" />
-            </div>    
-        </div>
+            </div>
+            <div className="images-row-desktop">
+                <img src="/images/desktop/image-gallery-milkbottles.jpg" alt="" />
+                <img src="/images/desktop/image-gallery-orange.jpg" alt="" />
+                <img src="/images/desktop/image-gallery-cone.jpg" alt="" />
+                <img src="/images/desktop/image-gallery-sugarcubes.jpg" alt="" />
+            </div>
+        </section>
     )
 }
