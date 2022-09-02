35fe7450faf7dc317bea4ccdc9294b3029e06aa8
oscar anillo <oscarani14@gmail.com>
2022-09-02 17:58:29 -0500

    add animation to the app images

--- components/images-component.js
+++ components/images-component.js
@@ -1,6 +1,27 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+gsap.registerPlugin(ScrollTrigger);
+
 export default function Images(){
+
+    useEffect(() => {
+        gsap.from(".desk-img", {
+          stagger: 0.3,
+          y: 100,
+          scale: 1.5,
+          duration: 2,
+          opacity: 0,
+          ease: "power4.inOut",
+          scrollTrigger: {
+            trigger: "#section-images",
+            toggleActions: "restart none none reset"
+          }
+        });
+      }, []);
+
     return (
-        <section>
+        <section id="section-images">
             <div className="images-row-mobile">
                 <img src="/images/mobile/image-gallery-milkbottles.jpg" alt="" className="mobile-img" />
                 <img src="/images/mobile/image-gallery-orange.jpg" alt="" className="mobile-img" />
@@ -8,10 +29,10 @@ export default function Images(){
                 <img src="/images/mobile/image-gallery-sugar-cubes.jpg" alt="" className="mobile-img" />
             </div>
             <div className="images-row-desktop">
-                <img src="/images/desktop/image-gallery-milkbottles.jpg" alt="" />
-                <img src="/images/desktop/image-gallery-orange.jpg" alt="" />
-                <img src="/images/desktop/image-gallery-cone.jpg" alt="" />
-                <img src="/images/desktop/image-gallery-sugarcubes.jpg" alt="" />
+                <img src="/images/desktop/image-gallery-milkbottles.jpg" alt="" className="desk-img" />
+                <img src="/images/desktop/image-gallery-orange.jpg" alt="" className="desk-img" />
+                <img src="/images/desktop/image-gallery-cone.jpg" alt="" className="desk-img" />
+                <img src="/images/desktop/image-gallery-sugarcubes.jpg" alt="" className="desk-img" />
             </div>
         </section>
     )
--- components/product.js
+++ components/product.js
@@ -1,4 +1,23 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect } from "react";
+gsap.registerPlugin(ScrollTrigger);
+
+
 export default function Product(){
+
+    useEffect(() => {
+        gsap.from(".product-bg-graphic, .product-bg-photo", {
+          y: "200",
+          duration: 1,
+          opacity: 0,
+          scrollTrigger: {
+            trigger: ".product",
+            toggleActions: "restart none reverse reset"
+          }
+        });
+      }, []);
+
     return (
         <div className="product">
             <div className="product-bg-graphic">
